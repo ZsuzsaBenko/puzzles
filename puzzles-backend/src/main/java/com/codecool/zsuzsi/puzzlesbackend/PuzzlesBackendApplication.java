30eff6515ee8bf25cd2a6ad41b84b57525bdb175
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-08-24 17:41:12 +0200

    Initialize git repository

---
+++ puzzles-backend/src/main/java/com/codecool/zsuzsi/puzzlesbackend/PuzzlesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.zsuzsi.puzzlesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuzzlesBackendApplication.class, args);
+    }
+
+}
