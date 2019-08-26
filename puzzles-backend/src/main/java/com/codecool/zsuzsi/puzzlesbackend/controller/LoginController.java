41fd3ad9837a87dd4d71b61081cf5912af5af3fb
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-08-26 09:37:02 +0200

    Implement login controller

--- puzzles-backend/src/main/java/com/codecool/zsuzsi/puzzlesbackend/controller/LoginController.java
+++ puzzles-backend/src/main/java/com/codecool/zsuzsi/puzzlesbackend/controller/LoginController.java
@@ -1,13 +1,52 @@
 package com.codecool.zsuzsi.puzzlesbackend.controller;
 
+import com.codecool.zsuzsi.puzzlesbackend.model.UserCredentials;
+import com.codecool.zsuzsi.puzzlesbackend.security.JwtTokenServices;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenServices jwtTokenServices;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody UserCredentials data) {
+        log.info("Login attempt: " + data.getEmail());
+        try {
+            String email = data.getEmail();
+            String password = data.getPassword();
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+            List<String> roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+            String token = jwtTokenServices.createToken(email, roles);
+            Map<Object, Object> model = new HashMap<>();
+            model.put("email", email);
+            model.put("roles", roles);
+            model.put("token", token);
+            return ResponseEntity.ok(model);
+
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid email / password supplied.");
+        }
+    }
 }
---
+++ puzzles-backend/src/main/java/com/codecool/zsuzsi/puzzlesbackend/model/UserCredentials.java
@@ -0,0 +1,15 @@
+package com.codecool.zsuzsi.puzzlesbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCredentials {
+    private String email;
+    private String password;
+}
