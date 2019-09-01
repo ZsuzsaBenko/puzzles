4f0ee930c37d552b1905156252b616f0384e90bf
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-01 23:24:34 +0200

    Implement registration (front-end)

--- puzzles-frontend/src/app/components/welcome/registration/registration.component.ts
+++ puzzles-frontend/src/app/components/welcome/registration/registration.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RegistrationService} from '../../../services/registration.service';
+import {NgForm} from '@angular/forms';
+import {Member} from '../../../models/Member';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  message = '';
+  submitted = false;
 
-  constructor() { }
+  constructor(private registrationService: RegistrationService) {
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    const member = new Member();
+    member.username = form.value['reg-username'];
+    member.email = form.value['reg-email'];
+    member.password = form.value['reg-password'];
+
+    this.registrationService.registerNewMember(member).subscribe((response) => {
+      if (response == null) {
+        this.message = 'Ezt az email címet már regisztrálták. Ha a Tiéd, jelentkezz be!';
+      } else {
+        this.message = `Kedves ${response.username}! Sikeresen regisztráltál. Jelentkezz be!`;
+      }
+      this.submitted = true;
+      form.reset();
+    });
+  }
 }
