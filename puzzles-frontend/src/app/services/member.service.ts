55c04d356b7a915a6ff0c5eb7441fe86bb495a1a
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-02 15:25:06 +0200

    Add MemberService

---
+++ puzzles-frontend/src/app/services/member.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Member } from '../models/Member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+
+  constructor(private http: HttpClient) { }
+
+  getLeaderBoard() {
+    const url = 'http://localhost:8080/members/leaderboard';
+    return this.http.get<Member[]>(url);
+  }
+}
