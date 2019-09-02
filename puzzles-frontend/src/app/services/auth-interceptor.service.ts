71bb52b63066872bc573cc2e836b338e8b58cac7
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-02 15:25:40 +0200

    Add AuthInterceptorService to send token with each request after login

---
+++ puzzles-frontend/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,15 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!req.url.endsWith('registration') && !req.url.endsWith('login')) {
+      const modifiedReq = req.clone({
+        headers: req.headers.append('Authorization', `Bearer ${localStorage.getItem('token')}`)
+      });
+      return next.handle(modifiedReq);
+    }
+    return next.handle(req);
+  }
+
+}
