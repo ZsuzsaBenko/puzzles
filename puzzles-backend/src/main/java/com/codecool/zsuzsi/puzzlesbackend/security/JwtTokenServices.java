d4bc1ec4e0cef64a2b331da87d868aab8ba281e1
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-06 11:15:09 +0200

    Change expiration date of token from 1 hour to 10 hours.

--- puzzles-backend/src/main/java/com/codecool/zsuzsi/puzzlesbackend/security/JwtTokenServices.java
+++ puzzles-backend/src/main/java/com/codecool/zsuzsi/puzzlesbackend/security/JwtTokenServices.java
@@ -23,8 +23,8 @@ public class JwtTokenServices {
     @Value("${security.jwt.token.secret-key:secret}")
     private String secretKey = "secret";
 
-    @Value("${security.jwt.token.expire-length:3600000}")
-    private long validityInMilliseconds = 36000000; // 10h
+    @Value("${security.jwt.token.expire-length:36000000}")
+    private long validityInMilliseconds = 360000000; // 10h
 
     private final String rolesFieldName = "roles";
 
