a9e35223826432e868b9bce5060833e52df524e8
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-09 22:34:57 +0200

    Remove commented-out part from SolutionServiceTest, add comment on scores, times

--- puzzles-backend/src/test/java/com/codecool/zsuzsi/puzzlesbackend/service/SolutionServiceTest.java
+++ puzzles-backend/src/test/java/com/codecool/zsuzsi/puzzlesbackend/service/SolutionServiceTest.java
@@ -61,6 +61,13 @@ class SolutionServiceTest {
 
     @Test
     public void testSaveSolution() {
+        /*
+        EASY_SCORE = 10;
+        MEDIUM_SCORE = 20;
+        MEDIUM_TIME_LIMIT = 120;
+        DIFFICULT_TIME_LIMIT = 300;
+         */
+
         Level initialLevel = Level.EASY;
         Level expectedLevel = Level.MEDIUM;
         List<Integer> solutionTimes = Arrays.asList(60, 150, 300);
@@ -100,78 +107,3 @@ class SolutionServiceTest {
         verify(solutionRepository).findAllByMember(member);
     }
 }
-
-/*
-
-    public Solution saveSolution(Solution solution, Member member) {
-        Puzzle solvedPuzzle = puzzleRepository.findById(solution.getPuzzle().getId()).orElse(null);
-
-        if (solvedPuzzle != null) {
-            solution.setPuzzle(solvedPuzzle);
-            solution.setMember(member);
-            solutionRepository.save(solution);
-
-            log.info("Member " + member.getEmail() + " saved solution for puzzle " + solvedPuzzle.getId() +
-                    "; seconds: " + solution.getSeconds() + ", rating: " + solution.getRating());
-
-            this.updateRating(solvedPuzzle);
-            this.updateScore(member);
-            this.updateLevel(solvedPuzzle);
-
-            return solution;
-        }
-        return null;
-    }
-
-    private Puzzle updateRating(Puzzle solvedPuzzle) {
-        double prevRating = solvedPuzzle.getRating();
-        double newRating = solutionRepository.getRatingAverage(solvedPuzzle);
-        solvedPuzzle.setRating(newRating);
-        puzzleRepository.save(solvedPuzzle);
-
-        log.info("Previous rating: " + prevRating + ", new rating: " + solvedPuzzle.getRating());
-
-        return solvedPuzzle;
-    }
-
-    private Puzzle updateLevel(Puzzle solvedPuzzle) {
-        Level prevLevel = solvedPuzzle.getLevel();
-        List<Integer> solutionTimes = solutionRepository.getSolutionTimes(solvedPuzzle);
-        double levelAverage = solutionTimes.stream()
-                .map(time -> time > HARD_TIME_LIMIT ? 2 : time > MEDIUM_TIME_LIMIT ? 1 : 0)
-                .mapToInt(Integer::intValue)
-                .average()
-                .orElse(0.0);
-
-        if (levelAverage <= 0.5) {
-            solvedPuzzle.setLevel(Level.EASY);
-        } else if (levelAverage <= 1.5) {
-            solvedPuzzle.setLevel(Level.MEDIUM);
-        } else {
-            solvedPuzzle.setLevel(Level.DIFFICULT);
-        }
-        puzzleRepository.save(solvedPuzzle);
-
-        log.info("Previous level: " + prevLevel + ", new level: " + solvedPuzzle.getLevel());
-
-        return solvedPuzzle;
-    }
-
-    private Member updateScore(Member member) {
-        int prevScore = member.getScore();
-        List<Solution> solutions = solutionRepository.findAllByMember(member);
-        int score = solutions.stream()
-                .map(solution -> solution.getPuzzle().getLevel().ordinal() + 1)
-                .map(num -> num == 1 ? EASY_SCORE : num == 2 ? MEDIUM_SCORE : HARD_SCORE)
-                .mapToInt(Integer::intValue)
-                .sum();
-        member.setScore(score);
-        memberRepository.save(member);
-
-        log.info("Member " + member.getEmail() + "'s previous score: " + prevScore +
-                ", new score: " + member.getScore());
-
-        return member;
-    }
-
- */
