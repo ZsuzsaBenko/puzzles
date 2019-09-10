bec41083924c906d7bbc10331bfeb074f7e259fc
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-09-10 22:11:52 +0200

    Test RegistrationController

---
+++ puzzles-backend/src/test/java/com/codecool/zsuzsi/puzzlesbackend/controller/RegistrationControllerTest.java
@@ -0,0 +1,59 @@
+package com.codecool.zsuzsi.puzzlesbackend.controller;
+
+import com.codecool.zsuzsi.puzzlesbackend.model.Member;
+import com.codecool.zsuzsi.puzzlesbackend.model.UserCredentials;
+import com.codecool.zsuzsi.puzzlesbackend.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class RegistrationControllerTest {
+
+    @MockBean
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testRegistration() throws Exception {
+        UserCredentials data = UserCredentials.builder()
+                .username("user").email("email@email.hu").password("password").build();
+        Member member = Member.builder().username("user").email("email@email.hu").password("password").build();
+        String requestBody = objectMapper.writeValueAsString(data);
+
+        when(memberService.register(data)).thenReturn(member);
+
+        MvcResult mvcResult = mockMvc
+                .perform(
+                        post("/registration")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseBody = mvcResult.getResponse().getContentAsString();
+
+        assertEquals(objectMapper.writeValueAsString(member), responseBody);
+        verify(memberService).register(data);
+    }
+}
